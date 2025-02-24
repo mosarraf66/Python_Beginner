abc853cbb11cedfdf7316ace441a0002bf5dc19b
Sk Abdul Mosarraf <157056597+mosarraf66@users.noreply.github.com>
2025-02-24 21:41:18 +0530

    first commit

---
+++ syntax.py
@@ -0,0 +1,22 @@
+'''print("i am sk abdul mosraf")
+print("i am sk abdul mosraf")
+print("i am sk abdul mosraf")
+print("i am sk abdul mosraf") '''
+
+'''x=8 
+y="Abdul"
+print(x)
+print(y)'''
+'''x=8
+y="Abdul"
+print(type(x))
+print(type(y))'''
+
+x=str(3)
+y=float(3)
+z=int(3)
+
+print(x)
+print(y)
+print(z)
+
